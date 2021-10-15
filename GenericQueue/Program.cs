f865766bf02340b5cdba1526f9ca45edd71f2e4e
vishal.Alok <you@example.com>
2021-10-15 05:56:26 +0530

    Add project files.

---
+++ GenericQueue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> Obj = new Queue<string>();
+            Obj.Enqueue("Hello1");
+            Obj.Enqueue("Hello2");
+            Obj.Enqueue("Hello3");
+
+            foreach (string s in Obj)
+            {
+                Console.WriteLine(s + " ");
+            }
+            Obj.Dequeue();
+            foreach (string s in Obj)
+            {
+                Console.WriteLine(s + " ");
+            }
+            Console.WriteLine(Obj.Peek());
+        }
+    }
+}
